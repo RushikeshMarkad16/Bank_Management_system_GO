d5eec97abac821f19364653125fe28d581c8b7d3
Rushi <rushikeshmarkad0@gmail.com>
2022-08-01 14:56:29 +0530

    Added the login functionality of the accountant and he can add the customers and generate account number and password for them

--- main.go
+++ main.go
@@ -1,41 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	//"Day5/Bank/Accountant"
+)
+
+type customer struct {
+	cust_email string
+	phone      int
+	account_no int
+	c_password int
+	balance    int
+}
+
+func generatePass(account_no int) {
+	c_password := rand.Intn(5000) + account_no
+	fmt.Println("Your account number is : ", account_no)
+	fmt.Println("Your password is : ", c_password)
+}
 
 //To login as a accountant or the customer
-func login(acc_email, acc_password string){
+func login(acc_email, acc_password string) {
 	fmt.Println("Hello, how you want to login")
 	fmt.Println("1. Accountant")
 	fmt.Println("2. Customer")
-	fmt.Println("3. Exit")
 	fmt.Println("Enter your choice : ")
 	var inp int
 	fmt.Scanln(&inp)
 
 	//login for accountant
-	if inp==1 {
+	if inp == 1 {
 		fmt.Println("Enter your mail id : ")
 		var a_login_mail string
 		fmt.Scanln(&a_login_mail)
 		fmt.Println("Enter your password : ")
 		var a_login_pass string
 		fmt.Scanln(&a_login_pass)
-		if a_login_mail==acc_email && a_login_pass==acc_password{
+
+		if a_login_mail == acc_email && a_login_pass == acc_password {
 			fmt.Println("Welcome, Logged in successfully as Accountant")
+
+			var phone int
+			fmt.Println("Enter customer's phone no : ")
+			fmt.Scanln(&phone)
+			var cust_email string
+			fmt.Println("Enter customer's email : ")
+			fmt.Scanln(&cust_email)
+			account_no := 320156851
+			generatePass(account_no)
+
+		} else {
+			//for incorrect credentials
+			fmt.Println("Access denied....please provide correct credentials")
+			login(acc_email, acc_password)
 		}
-	}else if inp==2{
-		//login for Customer
+
+	} else if inp == 2 {
 		fmt.Println("In customer loop")
-	}else {
-		//Exit
-		fmt.Println("In exit loop")
+
+	} else {
+		//for incorrect choice
+		fmt.Println("Please provide valid choice")
+		login(acc_email, acc_password)
 	}
-	
+
 }
 
-func main(){
-	acc_email := " account@bank.com"
+func main() {
+	acc_email := "account@bank.com"
 	acc_password := "josh@123"
-	login(acc_email,acc_password)
-	
+	login(acc_email, acc_password)
+
 }
